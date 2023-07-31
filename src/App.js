0c5300790e3d0eb24b5686128d1cbd7349df54eb
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-07-31 14:59:52 +0100

    imported the img logo inside my project and styled it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hjhjhjh
-        </a>
+      <header>
+        <span>
+          <img src={logo} alt="logo" width="200px" className="img-fluid" />
+        </span>
+        <span>
+          <h1>Dictionary</h1>
+        </span>
       </header>
     </div>
   );

7d8705f661c215f0d2189d50e621f871cfbef283
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-08-01 14:16:33 +0100

    updated Synonym: used map to loop through each synonym and display them

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+//created the component to display the synonyms by sending the data from the <Meaning/>  through properties to <Synonyms/>
+//sent the definition value,which is an array of strings as properties and render one synonym at a time
 export default function Synonyms(properties) {
   if (properties.synonyms) {
     return (
-      <div>
+      <ul>
         {properties.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}</span>;
+          return <li key={index}>{synonym}</li>;
         })}
-      </div>
+      </ul>
     );
   } else {
     return null;
